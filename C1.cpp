2d83d02029738446a7cc783eb4232e0268ecb0a6
VietNguyen <nguynviet2006@gmail.com>
2025-07-11 21:42:20 +0700

    Update C++

---
+++ C1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n != 1)
+        cout << "Hello world";
+    else
+        cout << "n = 1";
+    return 0;
+}
