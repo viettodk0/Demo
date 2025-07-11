118946245f65c5153281c186d56ed91bf8c95c7a
VietNguyen <nguynviet2006@gmail.com>
2025-07-11 22:06:07 +0700

    Add new branch

---
+++ C2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cin >> m;
+    if (m)
+        cout << "Hello world";
+    else
+        cout << "Hello Viet";
+    return 0;
+}
